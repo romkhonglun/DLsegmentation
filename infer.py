634b757e6fce6608fc3244d5eafa05a333505970
romkhonglun <cong.nh225476@sis.hust.edu.vn>
2024-11-23 23:41:25 +0700

    infer.py

--- infer.py
+++ infer.py
@@ -1,6 +1,5 @@
 import torch
 from torch import optim
-from collections import OrderedDict
 import segmentation_models_pytorch as smp
 import argparse
 from PIL import Image
@@ -8,11 +7,12 @@ import albumentations as A
 from albumentations.pytorch import ToTensorV2
 import numpy as np
 import matplotlib.pyplot as plt
-
 # Argument parsing
 parser = argparse.ArgumentParser(description='Inference script')
 parser.add_argument('--image_path', type=str, required=True, help='Path to the input image')
 args = parser.parse_args()
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Model setup
 model = smp.UnetPlusPlus(
@@ -28,17 +28,13 @@ optimizer = optim.Adam(params=model.parameters(), lr=learning_rate)
 checkpoint = torch.load("model.pth")
 optimizer.load_state_dict(checkpoint['optimizer'])
 
-new_state_dict = OrderedDict()
-for k, v in checkpoint['model'].items():
-    name = k[7:]  # remove `module.`
-    new_state_dict[name] = v
 # Load params
-model.load_state_dict(new_state_dict)
+model.load_state_dict(checkpoint['model'])
 model.eval()
+model.to(device)
 
 # Use the image_path argument
 image_path = args.image_path
-print(f"Image path provided: {image_path}")
 
 # Load the image
 image = Image.open(image_path)
@@ -54,10 +50,6 @@ transform = A.Compose([
 image_np = np.array(image)
 transformed = transform(image=image_np)
 input_tensor = transformed['image'].unsqueeze(0)  # Create a mini-batch as expected by the model
-
-# Move the input to the same device as the model
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model.to(device)
 input_tensor = input_tensor.to(device)
 
 # Perform inference
@@ -71,7 +63,7 @@ output = output.squeeze().cpu().numpy()
 output = np.transpose(output, (1, 2, 0))  # Change the shape to (H, W, C)
 output = (output - output.min()) / (output.max() - output.min())  # Normalize to [0, 1]
 output = (output * 255).astype(np.uint8)  # Scale to [0, 255] and convert to uint8
-
+output = Image.fromarray(output)
 # Display the input and output images side by side
 fig, axes = plt.subplots(1, 2, figsize=(12, 6))
 axes[0].imshow(image)
